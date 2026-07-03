5bb1be223e30069d6eea532ac613a51e4af1e4af
himmat2025 <himmat_rai@next.co.uk>
2026-07-03 22:58:22 +0100

    (initial commit): dockerfile for a celery worker service with a heavy compute task added along with compose for orchestrating redis and selery worker service containers

---
+++ tasks.py
@@ -0,0 +1,23 @@
+import time
+from collections import defaultdict
+from celery import Celery 
+
+app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
+
+@app.task
+def generate_k_square_numbers(k:int) -> list[int]:
+    print(f"[Worker] Starting heavy {k} square numbers computation...")
+    k_squares = []
+    n = 1
+    while n <= k:
+        time.sleep(0.5)
+        k_squares.append(n * n)
+        n += 1          
+    print(f"[Worker] Finished computation!\nResult: {k_squares}")
+    return k_squares
+
+# def main():
+#     print('Heavy computation task')
+
+# if __name__ == "__main__":
+#     main()
